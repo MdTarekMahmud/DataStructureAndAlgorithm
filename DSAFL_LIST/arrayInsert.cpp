d7711d15d24962192cc2bd4a3eac1d4f206785fe
Md Tarek Mahmud <112857212+MdTarekMahmud@users.noreply.github.com>
2023-09-20 18:28:33 +0600

    Update arrayInsert.cpp
    Using Switch

--- DSAFL_LIST/arrayInsert.cpp
+++ DSAFL_LIST/arrayInsert.cpp
@@ -1,90 +1,77 @@
 #include<iostream>
-#define Z 50
 using namespace std;
 void printArray(int arr[],int size)
 {
-    for(int i=0;i<size;i++){
+    for(int i=0;i<size;i++)
         cout << arr[i] << " ";
-    }
 }
-void insertBegining(int arr[],int & size, int value)
+void insertBegining(int arr[],int &size,int value)
 {
-    if(size+1<Z)
+    for(int i=size-1;i>=0;i--)
     {
-        for(int i=size-1;i>=0;i--){
-            arr[i+1]=arr[i];
-        }
-        arr[0]=value;
+        arr[i+1]=arr[i];
     }
+    arr[0]=value;
+    size++;
+}
+void insertMid(int arr[],int &size,int pos,int value)
+{
+    for(int i=size-1;i>=pos-1;i--)
+        arr[i+1]=arr[i];
+    arr[pos-1]=value;
+    size++;
+}
+void insertEnd(int arr[],int &size,int value)
+{
+    arr[size]=value;
     size++;
 }
 int main()
 {
-    int arr[Z]={1,2,3,4,5};
-    int size =5;
-    cout << "Array : " ;
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout << "Array before Insertion : ";
     printArray(arr,size);
     cout << endl;
-    insertBegining(arr,size,99);
-    printArray(arr,size);
+    int x;
+    do{
+        int value,pos;
+        cout << "\nChose an Option : " << endl;
+        cout << "1.Insert an element at the Begining." << endl;
+        cout << "2.Insert an element at the Midle." << endl;
+        cout << "3.Insert an element at the End." << endl;
+        cout << "4.For Exit" << endl;
+        cin >> x;
+        switch(x){
+        case 1:
+            cout << "Enter value : ";
+            cin >> value;
+            insertBegining(arr,size,value);
+            cout << endl;
+            cout << "Array After Insertion : ";
+            printArray(arr,size);
+            break;
+        case 2:
+            cout << "Enter position and value : ";
+            cin >> pos >> value;
+            insertMid(arr,size,pos,value);
+            cout << endl;
+            cout << "Array After Insertion : ";
+            printArray(arr,size);
+            break;
+        case 3:
+            cout << "Enter value : ";
+            cin >> value;
+            insertEnd(arr,size,value);
+            cout << endl;
+            cout << "Array After Insertion : ";
+            printArray(arr,size);
+            break;
+        case 4:
+            return 0;
+        default:
+            cout << "Invalid!";
+        }
+    }while(x!=4);
     return 0;
 }
-
-
-// #include<iostream>
-// using namespace std;
-// void printArray(int arr[],int size){
-// 	for (int i = 0; i < size; i++)
-// 		cout << arr[i] << " ";
-// 	cout << endl;
-// }
-// void insertMid(int arr[],int size,int pos, int value)
-// {
-//    int i;
-//     for (i = size - 1; i >= pos; i--)
-// 		arr[i] = arr[i - 1];
-// 	arr[pos - 1] = value;
-// }
-// int main()
-// {
-// 	int arr[] = { 2,3,4,5,6 };
-//    int size =5;
-// 	int i, val, pos;
-// 	printArray(arr,size);
-// 	cout << endl;
-//     cout << "Enter position and value : ";
-//     cin >> pos >> val;
-//     size++;
-//    insertMid(arr,size,pos,val);
-// 	printArray(arr,size);
-// 	return 0;
-// }
-
-
-
-// #include<iostream>
-// #define Z 50
-// using namespace std;
-// void printArray(int arr[],int size)
-// {
-//     for(int i=0;i<size;i++){
-//         cout << arr[i] << " ";
-//     }
-// }
-// void insertEnd(int arr[],int & size, int value)
-// {
-//    arr[size]=value;
-//    size++;
-// }
-// int main()
-// {
-//     int arr[Z]={1,2,3,4,5};
-//     int size =5;
-//     cout << "Array : " ;
-//     printArray(arr,size);
-//     cout << endl;
-//     insertEnd(arr,size,99);
-//     insertEnd(arr,size,88);
-//     printArray(arr,size);
-//     return 0;
-// }

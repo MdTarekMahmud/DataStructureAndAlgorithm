f37a84ac8c549375c9eac041438ec7a5fe305c90
Md Tarek Mahmud <112857212+MdTarekMahmud@users.noreply.github.com>
2023-09-20 18:53:09 +0600

    Update ArrayDeletion.cpp

--- P1/ArrayDeletion.cpp
+++ P1/ArrayDeletion.cpp
@@ -1,30 +1,70 @@
 #include<iostream>
 using namespace std;
-void printArray(int arr[],int size){
+void printArray(int arr[],int size)
+{
     for(int i=0;i<size;i++)
         cout << arr[i] << " ";
-    cout << endl;
+}
+void deleteBegining(int arr[],int &size)
+{
+    for(int i=1;i<=size;i++)
+    {
+        arr[i-1]=arr[i];
+    }
+    size--;
+}
+void deleteMid(int arr[],int &size,int pos)
+{
+    for(int i=pos;i<=size;i++)
+        arr[i-1]=arr[i];
+    size--;
+}
+void deleteEnd(int arr[],int &size)
+{
+    size--;
 }
 int main()
 {
-    int arr[]={2,3,4,5,6};
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
     int size = sizeof(arr)/sizeof(arr[0]);
-    cout << "Before Deletion :";
+    cout << "Array before Deletion : ";
     printArray(arr,size);
-    int pos;
-    cout << "Enter Position:";
-    cin >> pos;
-
-    if(pos>=1 && pos <=size){
-        for(int i=pos-1;i<size-1;i++)
-            arr[i]=arr[i+1];
-        size--;
-        printArray(arr,size);
-    }
-    else{
-        cout <<"Invalid Position!!!!";
-    }
-    
-
+    cout << endl;
+    int x;
+    do{
+        int pos;
+        cout << "\nChoose an option : " << endl;
+        cout << "1.Delete an element at the Begining." << endl;
+        cout << "2.Delete an element at the Midle." << endl;
+        cout << "3.Delete an element at the End." << endl;
+        cout << "4.For Exit" << endl;
+        cin >> x;
+        switch(x){
+        case 1:
+            deleteBegining(arr,size);
+            cout << endl;
+            cout << "Array After Deletion: ";
+            printArray(arr,size);
+            break;
+        case 2:
+            cout << "Enter position : ";
+            cin >> pos;
+            deleteMid(arr,size,pos);
+            cout << endl;
+            cout << "Array After Deletion : ";
+            printArray(arr,size);
+            break;
+        case 3:
+            deleteEnd(arr,size);
+            cout << endl;
+            cout << "Array After Deletion : ";
+            printArray(arr,size);
+            break;
+        case 4:
+            return 0;
+        default:
+            cout << "Invalid!";
+        }
+    }while(x!=4);
     return 0;
 }
